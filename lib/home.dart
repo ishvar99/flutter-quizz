4c3309f5fb221c57335fb43db822679f01f48312
ishvar99 <ishanvarshney99@gmail.com>
2020-05-15 19:23:18 +0530

    added questions and their answers

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'dart:core';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -7,6 +8,45 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int questionNumber=0;
+  List<Icon> scoreKeeper = [];
+  List<Map<dynamic,dynamic>> questions=[
+  {'statement':'Cyclones spin in a clockwise direction in the southern hemisphere','answer':true},
+{'statement':'Goldfish only have a memory of three seconds','answer':false},
+{'statement':'The Channel Tunnel is the longest rail tunnel in the world','answer':false},
+    {'statement':'The first tea bags were made of silk','answer':true},
+{'statement':'A woman has walked on the moon','answer':false},
+{'statement':'The Statue of Liberty was a gift from France','answer':true}
+  ];
+  void reset()
+  {
+    setState(() {
+      questionNumber=0;
+      scoreKeeper=[];
+    });
+  }
+  void updateScoreKeeper(ans){
+    setState(() {
+      if(questions[questionNumber]['answer']==ans) {
+        scoreKeeper.add(
+          Icon(
+            Icons.check,
+            color: Colors.green,
+          ),
+        );
+      }
+      else{
+        scoreKeeper.add(
+          Icon(
+            Icons.close,
+            color: Colors.red,
+          ),
+        );
+      }
+      if(++questionNumber==questions.length)
+        reset();
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +60,7 @@ class _HomeState extends State<Home> {
               child: Padding(
                 padding: const EdgeInsets.all(10.0),
                 child: Text(
-                  'This is where question text will go out',
+                  questions[questionNumber]['statement'],
                   textAlign: TextAlign.center,
                   style: Theme.of(context).textTheme.display1,
                 ),
@@ -33,7 +73,7 @@ class _HomeState extends State<Home> {
               Padding(
                 padding: const EdgeInsets.all(10.0),
                 child: FlatButton(
-                  onPressed: () => print('true'),
+                  onPressed: ()=>updateScoreKeeper(true),
                   child: Container(
                     child: Center(
                       child: Text(
@@ -49,6 +89,7 @@ class _HomeState extends State<Home> {
               Padding(
                 padding: const EdgeInsets.all(10.0),
                 child: FlatButton(
+                  onPressed: ()=> updateScoreKeeper(false),
                   child: Container(
                     child: Center(
                       child: Text(
@@ -67,13 +108,7 @@ class _HomeState extends State<Home> {
             padding:
                 const EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
             child: Row(
-              children: <Widget>[
-                Icon(
-                  Icons.check,
-                  color: Colors.green,
-                ),
-                Icon(Icons.close, color: Colors.red)
-              ],
+              children: scoreKeeper,
             ),
           )
         ],
