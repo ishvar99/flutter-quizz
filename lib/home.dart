8430e7793ebb975a5a688ef7773f1e0d909f863e
ishvar99 <ishanvarshney99@gmail.com>
2020-05-16 11:33:05 +0530

    almost completed the app

--- lib/home.dart
+++ lib/home.dart
@@ -8,34 +8,50 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int questionNumber=0;
+  int questionNumber = 0;
+  bool showScore = false;
   List<Icon> scoreKeeper = [];
-  List<Map<dynamic,dynamic>> questions=[
-  {'statement':'Cyclones spin in a clockwise direction in the southern hemisphere','answer':true},
-{'statement':'Goldfish only have a memory of three seconds','answer':false},
-{'statement':'The Channel Tunnel is the longest rail tunnel in the world','answer':false},
-    {'statement':'The first tea bags were made of silk','answer':true},
-{'statement':'A woman has walked on the moon','answer':false},
-{'statement':'The Statue of Liberty was a gift from France','answer':true}
+  List<Map<dynamic, dynamic>> questions = [
+    {
+      'statement':
+          'Cyclones spin in a clockwise direction in the southern hemisphere',
+      'answer': true
+    },
+    {
+      'statement': 'Goldfish only have a memory of three seconds',
+      'answer': false
+    },
+    {
+      'statement': 'The Channel Tunnel is the longest rail tunnel in the world',
+      'answer': false
+    },
+    {'statement': 'The first tea bags were made of silk', 'answer': true},
+    {'statement': 'A woman has walked on the moon', 'answer': false},
+    {
+      'statement': 'The Statue of Liberty was a gift from France',
+      'answer': true
+    }
   ];
-  void reset()
-  {
+  void reset() {
     setState(() {
-      questionNumber=0;
-      scoreKeeper=[];
+      showScore=false;
+      questionNumber = 0;
+      scoreKeeper = [];
     });
   }
-  void updateScoreKeeper(ans){
+  int calculateScore(){
+
+  }
+  void updateScoreKeeper(ans) {
     setState(() {
-      if(questions[questionNumber]['answer']==ans) {
+      if (questions[questionNumber]['answer'] == ans) {
         scoreKeeper.add(
           Icon(
             Icons.check,
             color: Colors.green,
           ),
         );
-      }
-      else{
+      } else {
         scoreKeeper.add(
           Icon(
             Icons.close,
@@ -43,76 +59,91 @@ class _HomeState extends State<Home> {
           ),
         );
       }
-      if(++questionNumber==questions.length)
-        reset();
+      if (++questionNumber == questions.length) {
+        setState(() {
+          showScore = true;
+        });
+      }
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade900,
       appBar: AppBar(backgroundColor: Colors.grey.shade900),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Expanded(
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Text(
-                  questions[questionNumber]['statement'],
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.display1,
-                ),
+      body: showScore
+          ? Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('YOUR SCORE',style:Theme.of(context).textTheme.display1,),
+                  FlatButton(child: Text('Play Again',style: Theme.of(context).textTheme.display1,),
+                      onPressed: () => reset())
+                ],
               ),
-            ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: FlatButton(
-                  onPressed: ()=>updateScoreKeeper(true),
-                  child: Container(
-                    child: Center(
-                      child: Text(
-                        'True',
-                        style: Theme.of(context).textTheme.display1,
-                      ),
-                    ),
-                    height: 100,
-                    color: Colors.green,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: FlatButton(
-                  onPressed: ()=> updateScoreKeeper(false),
-                  child: Container(
-                    child: Center(
-                      child: Text(
-                        'False',
-                        style: Theme.of(context).textTheme.display1,
-                      ),
-                    ),
-                    height: 100,
-                    color: Colors.red,
-                  ),
-                ),
-              )
-            ],
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
-            child: Row(
-              children: scoreKeeper,
-            ),
           )
-        ],
-      ),
+          : Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Expanded(
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        questions[questionNumber]['statement'],
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.display1,
+                      ),
+                    ),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: FlatButton(
+                        onPressed: () => updateScoreKeeper(true),
+                        child: Container(
+                          child: Center(
+                            child: Text(
+                              'True',
+                              style: Theme.of(context).textTheme.display1,
+                            ),
+                          ),
+                          height: 100,
+                          color: Colors.green,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: FlatButton(
+                        onPressed: () => updateScoreKeeper(false),
+                        child: Container(
+                          child: Center(
+                            child: Text(
+                              'False',
+                              style: Theme.of(context).textTheme.display1,
+                            ),
+                          ),
+                          height: 100,
+                          color: Colors.red,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 15.0, horizontal: 20.0),
+                  child: Row(
+                    children: scoreKeeper,
+                  ),
+                )
+              ],
+            ),
     );
   }
 }
